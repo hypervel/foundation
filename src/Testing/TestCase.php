6bef72aadefe32942c67ec7b8b3a2d09672a597c
fu <asd5242539@gmail.com>
2025-11-06 16:05:05 +0800

    feat: add flushAfterCreated method to clear callbacks in Coroutine class

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -121,6 +121,7 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
                 fn () => $this->callBeforeApplicationDestroyedCallbacks()
             );
             $this->flushApplication();
+            Coroutine::flushAfterCreated();
         }
 
         $this->afterApplicationCreatedCallbacks = [];
