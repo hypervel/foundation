dddbd37b3258a8696a4a1c980cebc479b0cffec4
Albert Chen <albert.cht@gmail.com>
2026-07-02 15:51:41 +0800

    chore: bump version to v0.3.24

--- src/Application.php
+++ src/Application.php
@@ -31,7 +31,7 @@ class Application extends Container implements ApplicationContract
      *
      * @var string
      */
-    public const VERSION = '0.3.23';
+    public const VERSION = '0.3.24';
 
     /**
      * The base path for the Hypervel installation.
